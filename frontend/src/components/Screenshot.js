985449a0c5f50d0016c1c37e79361f81705c5fac
kingtiger312 <kingtiger312@outlook.com>
2021-12-21 23:57:20 -0600

    Step-5: Add Carousel.

--- frontend/src/components/Screenshot.js
+++ frontend/src/components/Screenshot.js
@@ -1,6 +1,8 @@
 import { extend } from "jquery";
 import React, { Component} from "react";
 import '../App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Carousel from 'react-bootstrap/Carousel';
 
 class Screenshot extends Component {
     render() {
@@ -10,7 +12,36 @@ class Screenshot extends Component {
                     <p>Screenshots</p>
                 </div>
                 <div className="d-flex justify-content-center screen-image">
-                    <img src='images/worms.png' />
+                <Carousel>
+                    <Carousel.Item interval={1500}>
+                    <img
+                        className=""
+            src="images/worms.png"
+                        alt="Image One"
+                    />
+                    </Carousel.Item>
+                    <Carousel.Item interval={1500}>
+                    <img
+                        className=""
+            src="images/touranment-1.png"
+                        alt="Image Two"
+                    />
+                    </Carousel.Item>
+                    <Carousel.Item interval={1500}>
+                    <img
+                        className=""
+            src="images/touranment-2.png"
+                        alt="Image Two"
+                    />
+                    </Carousel.Item>
+                    <Carousel.Item interval={1500}>
+                    <img
+                        className=""
+            src="images/touranment-3.png"
+                        alt="Image Two"
+                    />
+                    </Carousel.Item>
+                </Carousel>
                 </div>
 
             </div>
--- frontend/src/components/Slider.js
+++ frontend/src/components/Slider.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import  '../App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Carousel from 'react-bootstrap/Carousel';
 
 class Slider extends Component {
     render() {
         return (
             <div className='first-slogan mt-3' id='#start'>
-                <p>Los gusanos en la zona de </p>
-                <p> una serpiente resbaladiza</p>
+                <Carousel>
+                    <Carousel.Item interval={1500}>
+                    <img
+                        className=""
+            src="images/First-Slide.png"
+                        alt="Image One"
+                    />
+                    </Carousel.Item>
+                    <Carousel.Item interval={1500}>
+                    <img
+                        className=""
+            src="images/Second-Slide.png"
+                        alt="Image Two"
+                    />
+                    </Carousel.Item>
+                </Carousel>
             </div>
         );
     }
